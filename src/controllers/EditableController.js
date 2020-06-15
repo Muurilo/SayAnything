557ab7b62cf374899c1a5f379a9b7e1876b3ff68
Murilo Cunha <murilo.henriquecunha551@gmail.com>
2020-06-15 16:33:10 -0300

    Say Page

--- src/controllers/EditableController.js
+++ src/controllers/EditableController.js
@@ -2,24 +2,25 @@ const EditModel = require("../models/Edit")
 
 module.exports = {
     edit: function (req, res) {
+        if(req.body.content === undefined) {
+            res.sendStatus(500);
+        }
+        EditModel.updateOne({name: 'maincontent'}, {name: 'maincontent', content: req.body.content}, {upsert: true}, function (error, result) {
+            if(error) {
+                res.sendStatus(500)
+            }
+            res.sendStatus(200)
+        });
+    },
+    get: function (req, res) {
         EditModel.find({name: 'maincontent'}, function (err, docs) {
             if(err) {
                 throw err;
             }
-            if(docs.length === 0) {
-                var newEdit = new EditModel({
-                    name: 'maincontent',
-                    content: 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa'
-                })
-                newEdit.save()
+            if(docs[0] === undefined) {
+                res.status(200).json({content: ""})
             }else {
-                EditModel.findOne({ name: 'maincontent' }, function (err, doc) {
-                    if (err) {
-                        throw err;
-                    }
-                    doc.content = 'aaaaaa';
-                    doc.save();
-                })
+                res.status(200).json({content: docs[0].content})
             }
         })
     }
--- src/routes.js
+++ src/routes.js
@@ -3,5 +3,6 @@ const Router = require("express").Router();
 const EditController = require("./controllers/EditableController")
 
 Router.post("/editable", EditController.edit)
+Router.get("/editable", EditController.get)
 
 module.exports = Router;
